3f85ba64ed6ecee45f844695efe56869c848e024
Samueldamil <damilareedun@gmail.com>
2023-04-11 15:15:09 -0700

    file

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-""" Using pymongo """
+""" MongoDB Operations with Python using pymongo """
 
 
 def top_students(mongo_collection):
-    """returns all students sorted by average score """
-
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted by average score """
     top_student = mongo_collection.aggregate([
         {
             "$project": {
                 "name": "$name",
-                "averageScore": {"$avg": "topics.score"}
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
         {"$sort": {"averageScore": -1}}
     ])
+
     return top_student

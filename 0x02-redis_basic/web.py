0a817e747e4cbfa31133536fc3c18d594c9c1956
Samueldamil <damilareedun@gmail.com>
2023-04-12 22:18:04 +0000

    file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
+"""web module
 """
-web cache and tracker
-"""
-import requests
 import redis
+from typing import Callable
+import requests
 from functools import wraps
 
-store = redis.Redis()
+client = redis.Redis()
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
+def url_count(method: Callable) -> Callable:
+    """url_count function
+    Args:
+        method[Callable]:
+    returns:
+        Callable:
+    """
+
     @wraps(method)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
+        """wrapper decorated function"""
+        client.incr(f"count:{url}")
+        cached = client.get(url)
+        if cached:
+            return cached.decode("utf-8")
+        client.setex(url, 10, method(url))
+        return method(url)
 
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
     return wrapper
 
 
-@count_url_access
+@url_count
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """get_page function
+    Args:
+        url[str]:
+    Returns:
+        str:
+    """
+    return requests.get(url).text
